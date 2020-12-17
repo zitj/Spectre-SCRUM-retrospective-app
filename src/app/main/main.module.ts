1d55bc5cd1227ab03c3e4c13f1c26a50f5603b2c
Aleksandar Veljkovic <1eyedea2eyedeas@gmail.com>
2020-12-17 09:02:54 +0100

    routes added to main

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -4,16 +4,32 @@ import { DashboardsComponent } from './dashboards/dashboards.component';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { TeamsComponent } from './teams/teams.component';
+import { StatisticsComponent } from './statistics/statistics.component';
 
 const routes = [
   {
     path: 'dashboards',
     component: DashboardsComponent,
   },
+  {
+    path: 'statistics',
+    component: StatisticsComponent,
+  },
+
+  {
+    path: 'teams',
+    component: TeamsComponent,
+  },
 ];
 
 @NgModule({
-  declarations: [DashboardsComponent, HeaderComponent],
+  declarations: [
+    DashboardsComponent,
+    HeaderComponent,
+    TeamsComponent,
+    StatisticsComponent,
+  ],
   imports: [CommonModule, MatToolbarModule, RouterModule.forChild(routes)],
 })
 export class MainModule {}
