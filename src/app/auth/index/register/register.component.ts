3d1b187906515a5a1837003de6e74b92d21c1908
Aleksandar Veljkovic <1eyedea2eyedeas@gmail.com>
2020-12-26 13:33:41 +0100

    added custom validation

--- src/app/auth/index/register/register.component.ts
+++ src/app/auth/index/register/register.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import {
+  FormGroup,
+  Validators,
+  AbstractControl,
+  ValidatorFn,
+} from '@angular/forms';
 import { FormBuilder } from '@angular/forms';
 import { UsersService } from '../../../users.service';
 import { Subscription } from 'rxjs';
@@ -14,7 +19,6 @@ import { RegisterSuccessComponent } from './register-success/register-success.co
 })
 export class RegisterComponent implements OnInit, OnDestroy {
   hidePassword: boolean = false;
-  emailCheck: boolean = false;
 
   constructor(
     public dialog: MatDialog,
@@ -26,12 +30,16 @@ export class RegisterComponent implements OnInit, OnDestroy {
   private postSub: Subscription = new Subscription();
   formGroup: FormGroup = new FormGroup({});
   users: User[] = [];
+  emailErrMsg: string = 'Email is required';
 
   ngOnInit(): void {
     this.formGroup = this.formBuilder.group({
       firstName: ['', [Validators.required]],
       lastName: ['', [Validators.required]],
-      email: ['', [Validators.required, Validators.email]],
+      email: [
+        '',
+        [Validators.required, Validators.email, this.uniqueEmail(this.users)],
+      ],
       password: ['', [Validators.required]],
     });
     this.getSub = this.usersService.getUsers().subscribe((data) => {
@@ -45,13 +53,6 @@ export class RegisterComponent implements OnInit, OnDestroy {
   }
 
   onSubmit(): void {
-    for (let user of this.users) {
-      if (user.email === this.formGroup.value.email) {
-        this.emailCheck = true;
-        return;
-      }
-    }
-
     this.postSub = this.usersService
       .postUser(this.formGroup.value)
       .subscribe((data) => {
@@ -59,5 +60,21 @@ export class RegisterComponent implements OnInit, OnDestroy {
           panelClass: 'registerSuccessContainer',
         });
       });
+  }
+
+  uniqueEmail(users: User[]): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: boolean } | null => {
+      for (let user of this.users) {
+        if (user.email == control.value) {
+          this.emailErrMsg = 'This email already exists';
+          return { uniqueEmail: false };
+        }
+      }
+      if (!control.value) {
+        this.emailErrMsg = 'Email is required';
+        return { uniqueEmail: false };
+      }
+      return null;
+    };
   }
 }
