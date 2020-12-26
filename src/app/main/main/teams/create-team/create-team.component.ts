422e9f27ba3798c03a8d8c8a2479e58e4c684630
Aleksandar Veljkovic <1eyedea2eyedeas@gmail.com>
2020-12-26 14:22:45 +0100

    custom validation for teams added

--- src/app/main/main/teams/create-team/create-team.component.ts
+++ src/app/main/main/teams/create-team/create-team.component.ts
@@ -1,10 +1,16 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { FormGroup, Validators } from '@angular/forms';
+import {
+  FormGroup,
+  Validators,
+  AbstractControl,
+  ValidatorFn,
+} from '@angular/forms';
 import { FormBuilder } from '@angular/forms';
 import { TeamsService } from '../../../../teams.service';
 import { Subscription } from 'rxjs';
 import { User } from '../../../../user';
+import { Team } from '../../../../team';
 import { Router } from '@angular/router';
 import { Location } from '@angular/common';
 import { UsersService } from '../../../../users.service';
@@ -25,28 +31,51 @@ export class CreateTeamComponent implements OnInit, OnDestroy {
     private usersService: UsersService
   ) {}
   users: Array<User> = [];
+  teams: Array<Team> = [];
 
-  private getSub: Subscription = new Subscription();
+  private getUsers: Subscription = new Subscription();
+  private getTeams: Subscription = new Subscription();
   private postSub: Subscription = new Subscription();
   formGroup: FormGroup = new FormGroup({});
+  teamNameErrMsg: string = 'Team name is required';
 
   ngOnInit(): void {
     this.formGroup = this.formBuilder.group({
-      name: ['', [Validators.required]],
+      name: ['', [Validators.required, this.uniqueTeamName(this.teams)]],
       industry: ['', [Validators.required]],
       creatorId: JSON.parse(localStorage.getItem('UserLoggedIn') || '{}').id,
       memberId: '',
       isAdmin: false,
     });
 
-    this.getSub = this.usersService.getUsers().subscribe((data) => {
+    this.getUsers = this.usersService.getUsers().subscribe((data) => {
       this.users = data;
+    });
+    this.getTeams = this.teamsService.getTeams().subscribe((data) => {
+      this.teams = data;
     });
   }
 
   ngOnDestroy(): void {
     this.postSub.unsubscribe();
-    this.getSub.unsubscribe();
+    this.getUsers.unsubscribe();
+    this.getTeams.unsubscribe();
+  }
+
+  uniqueTeamName(teams: Team[]): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: boolean } | null => {
+      for (let team of this.teams) {
+        if (team.name.toLowerCase() == control.value.toLowerCase()) {
+          this.teamNameErrMsg = 'This team name already exists';
+          return { uniqueTeamName: false };
+        }
+      }
+      if (!control.value) {
+        this.teamNameErrMsg = 'Team name is required';
+        return { uniqueTeamName: false };
+      }
+      return null;
+    };
   }
 
   selectMembers(event: any): void {
