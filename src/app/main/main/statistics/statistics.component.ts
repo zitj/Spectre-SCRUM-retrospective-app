bc558e48b62480be466549f968a60e6e217c45ad
Aleksandar Veljkovic <1eyedea2eyedeas@gmail.com>
2021-01-04 21:19:54 +0100

    added basic team  statistics

--- src/app/main/main/statistics/statistics.component.ts
+++ src/app/main/main/statistics/statistics.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { TeamsService } from '../../../teams.service';
+import { DashboardsService } from '../../../dashboards.service';
+import { Team } from '../../../team';
+import { User } from '../../../user';
+import { Dashboard } from '../../../dashboard';
 
 @Component({
   selector: 'app-statistics',
@@ -6,7 +12,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./statistics.component.scss'],
 })
 export class StatisticsComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+  constructor(
+    private teamsService: TeamsService,
+    private dashboardsService: DashboardsService
+  ) {}
+  private getTeams: Subscription = new Subscription();
+  private getDashboards: Subscription = new Subscription();
+  teams: Team[] = [];
+  dashboards: Dashboard[] = [];
+  showDashboards: Dashboard[] = [];
+  userLoggedIn: User = JSON.parse(localStorage.getItem('UserLoggedIn') || '{}');
+  ngOnInit(): void {
+    this.getTeams = this.teamsService.getTeams().subscribe((data) => {
+      this.teams = data;
+    });
+  }
 }
